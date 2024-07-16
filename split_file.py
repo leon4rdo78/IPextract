fd8ec149d7dc015f59f05e9c51261639222339a1
kofukuDESU <95756688+leon4rdo78@users.noreply.github.com>
2024-07-16 18:32:05 +0330

    Update split_file.py

--- split_file.py
+++ split_file.py
@@ -2,7 +2,7 @@ import os
 import math
 import random  # Import the random module to shuffle lines
 
-def split_file(input_file, max_lines=500):
+def split_file(input_file, output_dir='split', max_lines=500):
     with open(input_file, 'r') as file:  # Open the input file for reading
         lines = file.readlines()  # Read all lines into a list
 
@@ -11,11 +11,14 @@ def split_file(input_file, max_lines=500):
     total_lines = len(lines)  # Get the total number of lines
     num_files = math.ceil(total_lines / max_lines)  # Calculate the number of output files needed
 
+    # Ensure the output directory exists
+    os.makedirs(output_dir, exist_ok=True)
+
     for i in range(num_files):  # Iterate over the number of files to create
         start = i * max_lines  # Calculate the start index for the current file
         end = min((i + 1) * max_lines, total_lines)  # Calculate the end index
 
-        output_file = "'split'/f'proxies_part_{i+1}.txt'"  # Define the output file name
+        output_file = os.path.join(output_dir, f'proxies_part_{i+1}.txt')  # Define the output file name
         with open(output_file, 'w') as file:  # Open the output file for writing
             file.writelines(lines[start:end])  # Write the subset of lines to the output file
         
